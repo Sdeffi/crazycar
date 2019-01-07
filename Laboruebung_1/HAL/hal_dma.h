c1a548f60ae816caf7e081d3e4bd4b39c6f82a92
Sdeffi <45669958+Sdeffi@users.noreply.github.com>
2019-01-07 17:37:11 +0100

    Create hal_dma.h

---
+++ Laboruebung_1/HAL/hal_dma.h
@@ -0,0 +1,15 @@
+/*
+ * hal_dma.h
+ *
+ *  Created on: 07.01.2019
+ *      Author: Steffi
+ */
+
+#ifndef HAL_HAL_DMA_H_
+#define HAL_HAL_DMA_H_
+
+
+void HAL_DMA_Init(void);
+
+
+#endif /* HAL_HAL_DMA_H_ */
